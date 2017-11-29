44bcb7ffd52dd4ddff9f7a8f3b274823611e68bc
Victor <vb4007@gmail.com>
2017-11-29 20:46:23 +0100

    Avec spotlight

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ app.get('/dbpedia/annotate', function (req, res) {
   dbpedia.annotate(req.query.text, function(error, response, body) {res.send(body)})
 })
 
+app.get('/dbpedia/spotlight', function (req, res) {
+  console.log(req.query.text)
+  dbpedia.spotlight(req.query.text, function(error, response, body) {res.send(body)})
+})
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
