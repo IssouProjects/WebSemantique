19a24434c2dbdd5f0e89008b44ddf6a44c13d20b
Victor <vb4007@gmail.com>
2017-12-05 20:04:54 +0100

    One more assert for the extract test

--- test/test-extract.js
+++ test/test-extract.js
@@ -12,6 +12,7 @@ describe('Extract', function() {
     it('should correctly give all the URIs with the correct occurences', (done) => {
         extract.results('France', function(occurences) {
             occurences.should.be.a('Map')
+            occurences.get("http://dbpedia.org/resource/France").should.equals(17)
             done()
         })
     })
