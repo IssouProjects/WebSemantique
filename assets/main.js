fab62b1cc9a6fdb4f02c1cdb771506e0e58d21a9
Vincent Redouté <vincent.redoute@live.fr>
2017-12-14 16:47:36 +0100

    Un peu de mise en page

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,13 @@
 $("#search").click(function () {
     if(document.getElementById("query").value === '') {return}
+    document.getElementById("genre").innerHTML = '';
+    document.getElementById("developer").innerHTML = '';
+    document.getElementById("name").innerHTML = '';
+    document.getElementById("releaseDate").innerHTML = '';
+    document.getElementById("wikipage").innerHTML = '';
+    document.getElementById("mode").innerHTML = '';
+    document.getElementById("publisher").innerHTML = '';
+    document.getElementById("platform").innerHTML = '';
     $.get({
         url: 'search',
         type: 'GET',
